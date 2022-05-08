b7ee2a4a8d0afc051f4dea1aa8dd60398b2b6e8f
unknown <pplusmessage@gmail.com>
2022-05-08 16:00:00 +0900

    slider css 수정 및 기능 수정

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState, useRef } from 'react'
 import styles from './Components.module.scss'
-// import { cx } from '../styles'
-
-const rangeArr = [1,25,50,75,100]
+import { cx } from '../styles'
 
 export default function Slider(){
   const [initialMousePosX, setInitialMousePosx] = useState(0)
@@ -15,7 +13,6 @@ export default function Slider(){
       document.removeEventListener('mousemove', isMoving)
     })
   })
-
   const isMoving = (e) => {
     const progressBarWidth = progressBar.current.offsetWidth
     let diffX = e.clientX - initialMousePosX
@@ -29,30 +26,30 @@ export default function Slider(){
     setInitialMousePosx(e.clientX - offsetX)
     document.addEventListener('mousemove', isMoving) 
   }
+
   const onClickPercent = (e) => {
-    if(e.target === e.currentTarget) return
+
     let posX
-    switch (e.target.className) {
-      case 'spot_1':
+    switch (e.currentTarget.value) {
+      case '1':
         posX = 0
         break
-      case 'spot_25':
+      case '25':
         posX = 85
         break
-      case 'spot_50':
+      case '50':
         posX = 170
         break
-      case 'spot_75':
+      case '75':
         posX = 255
         break
-      case 'spot_100':
+      case '100':
         posX = 340
         break
       default :
         return
     }
-    const percent = e.target.innerText.replace('%', '')
-    setPercentage(percent)
+    setPercentage(e.currentTarget.value)
     setOffsetX(posX)
   }
 
@@ -65,17 +62,11 @@ export default function Slider(){
       <div className={styles.inner}>
         <div className={styles.progress} ref = {progressBar}>
           <div className={styles.spotWrap}>
-            {
-              rangeArr.map(v => {
-                return (
-                  <span 
-                    key={v.toString()} 
-                    className={styles.spot}>
-                    {v}
-                  </span>
-                )
-              })
-            }
+            <span className={cx(styles.spot1, {[styles.active]:percentage >= 1 })}/>
+            <span className={cx(styles.spot25, {[styles.active]:percentage >= 25 })}/>
+            <span className={cx(styles.spot50, {[styles.active]:percentage >= 50 })}/>
+            <span className={cx(styles.spot75, {[styles.active]:percentage >= 75 })}/>
+            <span className={cx(styles.spot100, {[styles.active]:percentage >= 100 })}/>
           </div>
           <div 
             className={styles.progressBar} 
@@ -89,13 +80,13 @@ export default function Slider(){
           >move
           </button>
         </div>
-        <button 
-          type="button" 
-          className={styles.percent} 
-          onClick={onClickPercent}
-        >
-          {rangeArr.map(v => <span key={v.toString()} className={`styles.spot${v}`}>{`${v}%`}</span>)}
-        </button>
+        <div className={styles.buttonWrap}>
+          <button type="button" className={styles.spot1} onClick={onClickPercent} value="1">1%</button>
+          <button type="button" className={styles.spot25} onClick={onClickPercent} value="25">25%</button>
+          <button type="button" className={styles.spot50} onClick={onClickPercent} value="50">50%</button>
+          <button type="button" className={styles.spot75} onClick={onClickPercent} value="75">75%</button>
+          <button type="button" className={styles.spot100} onClick={onClickPercent} value="100">100%</button>
+        </div>
       </div>
     </div>
   )
