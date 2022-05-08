900ab2048dc002082a92c041c043b7e5ad4fbf04
unknown <pplusmessage@gmail.com>
2022-05-08 18:59:33 +0900

    slider range 로 수정

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,34 +1,34 @@
-const {useState} = React;
+// const {useState} = React;
 
-const Dropdown = () => {
-  const [searchBox, setSearchBox] = useState('');
+// const Dropdown = () => {
+//   const [searchBox, setSearchBox] = useState('');
 
-  const listWrapControl = () => {
-    if(searchBox) setSearchBox('');
-    else setSearchBox('active');
-  };
+//   const listWrapControl = () => {
+//     if(searchBox) setSearchBox('');
+//     else setSearchBox('active');
+//   };
 
 
-  return(
-    <div className="drop_down_container">
-     <button type="button" className="current icon" onClick={listWrapControl}>All Symbols</button>
-     <div className={`list_wrap ${searchBox}`}>
-      <div className="search_box icon">
-        <input type="text" placeholder="Search Symbol"/>
-      </div>
-      <ul className="list">
-        <li>All Symbols</li>
-        <li>BTCUSD.PERP</li>
-        <li>BCHUSD.PERP</li>
-        <li>KWTHJDK.PERP</li>
-        <li>WOGHDJK.PERP</li>
-        <li>VWJORJE.PERP</li>
-        <li>1004ETEWE.PERP</li>
-        <li>QGDGRPG.PERP</li>
-      </ul>
-     </div>
-    </div>
-  )
-};
+//   return(
+//     <div className="drop_down_container">
+//      <button type="button" className="current icon" onClick={listWrapControl}>All Symbols</button>
+//      <div className={`list_wrap ${searchBox}`}>
+//       <div className="search_box icon">
+//         <input type="text" placeholder="Search Symbol"/>
+//       </div>
+//       <ul className="list">
+//         <li>All Symbols</li>
+//         <li>BTCUSD.PERP</li>
+//         <li>BCHUSD.PERP</li>
+//         <li>KWTHJDK.PERP</li>
+//         <li>WOGHDJK.PERP</li>
+//         <li>VWJORJE.PERP</li>
+//         <li>1004ETEWE.PERP</li>
+//         <li>QGDGRPG.PERP</li>
+//       </ul>
+//      </div>
+//     </div>
+//   )
+// };
 
-export default Dropdown;
+// export default Dropdown;
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,92 +1,40 @@
-import { useEffect, useState, useRef } from 'react'
+import { useState } from 'react'
 import styles from './Components.module.scss'
 import { cx } from '../styles'
 
 export default function Slider(){
-  const [initialMousePosX, setInitialMousePosx] = useState(0)
-  const [offsetX, setOffsetX] = useState(0)
-  const [percentage, setPercentage] = useState(1)
-  const progressBar = useRef(null)
-
-  useEffect(() => {
-    document.addEventListener('mouseup', () => {
-      document.removeEventListener('mousemove', isMoving)
-    })
-  })
-  const isMoving = (e) => {
-    const progressBarWidth = progressBar.current.offsetWidth
-    let diffX = e.clientX - initialMousePosX
-    if(diffX >= progressBarWidth) diffX = progressBarWidth
-    else if(diffX <= 0) diffX = 1
-    
-    setOffsetX(diffX)
-    setPercentage(Math.ceil(offsetX / progressBarWidth * 100))
-  }
-  const isMouseDown = (e) => {
-    setInitialMousePosx(e.clientX - offsetX)
-    document.addEventListener('mousemove', isMoving) 
-  }
+  const [value, setValue] = useState(1)
 
   const onClickPercent = (e) => {
-
-    let posX
-    switch (e.currentTarget.value) {
-      case '1':
-        posX = 0
-        break
-      case '25':
-        posX = 85
-        break
-      case '50':
-        posX = 170
-        break
-      case '75':
-        posX = 255
-        break
-      case '100':
-        posX = 340
-        break
-      default :
-        return
-    }
-    setPercentage(e.currentTarget.value)
-    setOffsetX(posX)
+    setValue(e.target.value)
   }
 
-  
+  const onChangeValue = (e) => {
+    setValue(e.target.value)
+  }
+
   return (
     <div className={styles.slider}>
       <div className={styles.percentBox}>
-        <span className={styles.number}>{percentage}</span>
+        <span className={styles.number}>{value}</span>
       </div>
       <div className={styles.inner}>
-        <div className={styles.progress} ref = {progressBar}>
-          <div className={styles.spotWrap}>
-            <span className={cx(styles.spot1, {[styles.active]:percentage >= 1 })}/>
-            <span className={cx(styles.spot25, {[styles.active]:percentage >= 25 })}/>
-            <span className={cx(styles.spot50, {[styles.active]:percentage >= 50 })}/>
-            <span className={cx(styles.spot75, {[styles.active]:percentage >= 75 })}/>
-            <span className={cx(styles.spot100, {[styles.active]:percentage >= 100 })}/>
-          </div>
-          <div 
-            className={styles.progressBar} 
-            style={{ width: `${percentage}%`}}
-          />
-          <button 
-            type="button"
-            className={styles.controller} 
-            onMouseDown={isMouseDown} 
-            style={{ left: `${percentage}%`}}
-          >move
-          </button>
+        <span className={styles.progressBar} style={{width:`${value}%`}}/>
+        <input type="range" id="points" name="points" min="1" max="100" value={value} onChange={onChangeValue}/>
+        <div className={styles.spotWrap}>
+          <span className={cx(styles.spot1, {[styles.active]:value > 1 })}/>
+          <span className={cx(styles.spot25, {[styles.active]:value > 25 })}/>
+          <span className={cx(styles.spot50, {[styles.active]:value > 50 })}/>
+          <span className={cx(styles.spot75, {[styles.active]:value > 75 })}/>
+          <span className={cx(styles.spot100, {[styles.active]:value > 100 })}/>
         </div>
-        <div className={styles.buttonWrap}>
-          <button type="button" className={styles.spot1} onClick={onClickPercent} value="1">1%</button>
-          <button type="button" className={styles.spot25} onClick={onClickPercent} value="25">25%</button>
-          <button type="button" className={styles.spot50} onClick={onClickPercent} value="50">50%</button>
-          <button type="button" className={styles.spot75} onClick={onClickPercent} value="75">75%</button>
-          <button type="button" className={styles.spot100} onClick={onClickPercent} value="100">100%</button>
-        </div>
+      </div>
+      <div className={styles.buttonWrap}>
+        <button type="button" className={styles.spot1} onClick={onClickPercent} value="1">1%</button>
+        <button type="button" className={styles.spot25} onClick={onClickPercent} value="25">25%</button>
+        <button type="button" className={styles.spot50} onClick={onClickPercent} value="50">50%</button>
+        <button type="button" className={styles.spot75} onClick={onClickPercent} value="75">75%</button>
+        <button type="button" className={styles.spot100} onClick={onClickPercent} value="100">100%</button>
       </div>
     </div>
   )
